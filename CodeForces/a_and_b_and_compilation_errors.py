fc617f8a6d2bb3d5fbf03f30458f94584e692369
root <gaurav.tolani@impactanalytics.co>
2018-11-13 14:06:10 +0530

    a_and_b_and_compilation_errors codeforces accepted code

---
+++ CodeForces/a_and_b_and_compilation_errors.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+n = int(input())
+errors_list = input().split()
+errors_list_1 = input().split()
+errors_list_2 = input().split()
+
+
+def list_difference(a, b):
+    count = defaultdict(int)
+    for x in a:
+        count[x] += 1
+
+    for x in b:
+        count[x] -= 1
+
+    diff = []
+    for x in a:
+        if count[x] > 0:
+           count[x] -= 1
+           diff.append(x)
+    return ''.join(diff)
+
+
+print(list_difference(errors_list, errors_list_1))
+print(list_difference(errors_list_1, errors_list_2))
